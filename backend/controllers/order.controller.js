334b90869ebc59ca033ba20fbba848191b378f21
atish-57 <128183388+atish-57@users.noreply.github.com>
2024-07-05 02:14:59 +0530

    Update order.controller.js

--- backend/controllers/order.controller.js
+++ backend/controllers/order.controller.js
@@ -8,7 +8,7 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY)
 
 const placeOrder = async (req, res) => {
 
-    const frontend_url = process.env.FRONTEND_URL;
+    const frontend_url = 'https://tomato-users.netlify.app';
 
     try {
         const newOrder = new orderModel({
