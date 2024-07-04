49a5a0242c8b50e2273e06a169f3d33a2fc3094a
Atish <1214atish@gmail.com>
2024-07-04 16:29:38 +0530

    added token expiry

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -28,7 +28,7 @@ const loginUser = async (req, res) => {
 }
 
 const createToken = (id) => {
-    return jwt.sign({ id }, process.env.JWT_SECRET);
+    return jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: '1m' });
 }
 
 const registerUser = async (req, res) => {
